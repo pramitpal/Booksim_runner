ea9032e5356af7e7892bc8a73741dbc7ec3a2ce5
Pramit Kumar Pal <41202066+pramitpal@users.noreply.github.com>
2026-01-30 16:14:49 -0800

    Replace booksim with booksim_new in script

--- run_chiplet_level.sh
+++ run_chiplet_level.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 chiplet_id=$1
-./booksim Chiplet_level_config > out_chiplet_${chiplet_id}.txt
+./booksim_new Chiplet_level_config > out_chiplet_${chiplet_id}.txt
 #echo "BookSim output saved for NoC to out_chiplet_${chiplet_id}.txt"
 echo -ne "\033[1A\033[2K"
